d45fa314d92799bc825c3911ff5b2b4a9839fefb
Alan Wilson <aww@umich.edu>
2013-05-23 23:35:29 +0200

    Solve matrix multiply - assignment 3 done

--- assignment3/6_multiply.py
+++ assignment3/6_multiply.py
@@ -2,6 +2,7 @@
 
 import MapReduce
 import sys
+import collections
 
 """
 Matrix multiply
@@ -16,20 +17,27 @@ def mapper(record):
     col = record[2]
     val = record[3]
     if which == "a":
-        for i in range(5):
-            mr.emit_intermediate((row, i), (col, val))
+        for i in range(size):
+            mr.emit_intermediate((row, i), (which, col, val))
     elif which == "b":
-        for i in range(5):
-            mr.emit_intermediate((i, col), (row, val))
+        for i in range(size):
+            mr.emit_intermediate((i, col), (which, row, val))
     
 def reducer(cell, idxval_list):
-    prod = 0
-    # Form a list of values paired by index
+    # build index
+    components = collections.defaultdict(list)
+    for which, i, val in idxval_list:
+        components[i].append(val)
 
-    for idx, val in idxval_list:
-        prod += val
-    if prod != 0:
-        mr.emit( (cell[0], cell[1], prod) )
+    # Compute product
+    sumprod = 0
+    for i,val_list in components.items():
+        if len(val_list) == 2:
+            sumprod += val_list[0] * val_list[1]
+        elif len(val_list) > 2:
+            raise Exception("Term (%d, %d, %d) has more than 2 factors!" % (cell[0], cell[1], i))
+    if sumprod != 0:
+        mr.emit( (cell[0], cell[1], sumprod) )
 
 if __name__ == '__main__':
   inputdata = open(sys.argv[1])
